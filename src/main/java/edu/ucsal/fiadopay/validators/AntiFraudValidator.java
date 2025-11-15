0d85f4b44eaea194d9bc7e1592defa7a2a218a9e
pena <lucaspsantos003@gmail.com>
2025-11-15 14:37:18 -0300

    feature(AntiFraudValidator): Criação da lógica de validar possíveis fraudes nos pagamentos

---
+++ src/main/java/edu/ucsal/fiadopay/validators/AntiFraudValidator.java
@@ -0,0 +1,25 @@
+package edu.ucsal.fiadopay.validators;
+
+import edu.ucsal.fiadopay.domain.Payment;
+import edu.ucsal.fiadopay.strategies.PaymentStategy;
+
+import java.math.BigDecimal;
+
+public class AntiFraudValidator implements PaymentStategy {
+    private final PaymentStategy paymentStategy;
+    private final double threshold;
+
+    public AntiFraudValidator(PaymentStategy paymentStategy, double threshold) {
+        this.paymentStategy = paymentStategy;
+        this.threshold = threshold;
+    }
+
+    @Override
+    public Payment.Status process(Payment payment) {
+        if(payment.getAmount().compareTo(BigDecimal.valueOf(this.threshold)) > 0) {
+            return Payment.Status.APPROVED;
+        } else {
+           return Payment.Status.DECLINED;
+        }
+    }
+}
