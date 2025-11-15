6181106b4b2ccc9ade3d82b58808f97cda0d4571
pena <lucaspsantos003@gmail.com>
2025-11-15 17:32:50 -0300

    feature(AuthorizePaymentUseCase): Criada a lógica para validação de pagamentos pendentes
    Recebe um paymentId que permite buscar o pagamento pendente no banco de dados e usar a fábrica de strategies para autorizá-lo e salva-lo novamente no banco

--- src/main/java/edu/ucsal/fiadopay/usecases/AuthorizePaymentUseCase.java
+++ src/main/java/edu/ucsal/fiadopay/usecases/AuthorizePaymentUseCase.java
@@ -1,8 +1,26 @@
 package edu.ucsal.fiadopay.usecases;
 
+import edu.ucsal.fiadopay.domain.Payment;
+import edu.ucsal.fiadopay.factories.PaymentStrategyFactory;
+import edu.ucsal.fiadopay.repo.PaymentRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AuthorizePaymentUseCase {
+    private final PaymentStrategyFactory paymentStrategyFactory;
+    private final PaymentRepository paymentRepository;
 
+    public AuthorizePaymentUseCase(PaymentStrategyFactory paymentStrategyFactory, PaymentRepository paymentRepository) {
+        this.paymentStrategyFactory = paymentStrategyFactory;
+        this.paymentRepository = paymentRepository;
+    }
+    @Transactional
+    public void execute(String paymentId) {
+        var payment = paymentRepository.getOne(paymentId);
+        var strategy = paymentStrategyFactory.getStrategy(payment.getMethod());
+        var newStatus = strategy.process(payment);
+        payment.setStatus(newStatus);
+        paymentRepository.save(payment);
+    }
 }
